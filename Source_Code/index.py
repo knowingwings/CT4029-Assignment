3758edcaca03afee4d63bb03946f6356ca0e6d65
knowingwings <thomas.lehuray@outlook.com>
2022-10-28 08:54:33 +0100

    Added Register page fully, created admin; customer; and staff views for dashboard not finsihed. Added temp funcs to access each dashboard

--- Source_Code/index.py
+++ Source_Code/index.py
@@ -1,8 +1,10 @@
-from mimetypes import init
+from atexit import register
 import tkinter as tk
-from tkinter import ttk
+from tkinter import FLAT, SOLID, UNDERLINE, StringVar, ttk, messagebox
+import re
 import sqlite3
-from tkinter.tix import Form
+from tkinter.font import ITALIC
+from turtle import color
 
 class window(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -23,7 +25,7 @@ class window(tk.Tk):
         #dictionary of frames
         self.frames = {}
         # add the components to the dictionary.
-        for F in (LoginPage, Dashboard, RegisterPage):
+        for F in (LoginPage, RegisterPage, Dashboard_Admin, Dashboard_Staff, Dashboard_Customer):
             frame = F(container, self)
 
             # the windows class acts as the root window for the frames.
@@ -50,13 +52,35 @@ class LoginPage(tk.Frame):
 
         userLabel = ttk.Label(
             self,
-            text="Username",
+            text="Email:",
             font=("Arial", 25)
         )
-        userLabel.place(x=1230, y=200)
+        userLabel.place(x=1300, y=200)
+
+        usernameInput = ttk.Entry(
+            self,
+            textvariable=StringVar,
+            font=("Arial", 25)
+        )
+        usernameInput.place(x=1300,y=250)
 
 
-        mIMG = tk.PhotoImage(file='./assets/loginPage/loginImage.png')
+        passwordLabel = ttk.Label(
+            self,
+            text="Password:",
+            font=("Arial", 25)
+        )
+        passwordLabel.place(x=1300, y=300)
+
+        passwordInput = ttk.Entry(
+            self,
+            textvariable=StringVar,
+            font=("Arial", 25),
+            show="•"
+        )
+        passwordInput.place(x=1300,y=350)
+
+        mIMG = tk.PhotoImage(file='./assets/branding/loginImage.png')
         mainImg = ttk.Label(
             self,
             image=mIMG)
@@ -65,23 +89,136 @@ class LoginPage(tk.Frame):
 
 
         # switch_window_button in order to call the show_frame() method as a lambda function
-        switch_window_button = tk.Button(
+        loginButton = tk.Button(
             self,
-            text="Go to the Side Page",
-            command=lambda: login(self, controller),
+            text="Login",
+            command=lambda: login(self, controller, usernameInput.get(), passwordInput.get()),
+            font=("Arial", 25),
+            border=False,
+            bg="light gray",
+            relief=FLAT           
         )
-        switch_window_button.pack(side="bottom", fill=tk.X)
+        loginButton.place(x=1420, y=400)
 
-        def login(self, controller):
-            print("LOGIN PRESSED")
-            controller.show_frame(Dashboard)
-            
+        registerButton = tk.Button(
+            self,
+            text="Create new Account",
+            command=lambda: controller.show_frame(RegisterPage),
+            font=("Arial", 8, UNDERLINE),
+            fg="blue",
+            border=False,
+            relief=FLAT           
+        )
+        registerButton.place(x=1420, y=475)
+
+        def login(self, controller, username, password):
+            if(username=="" or password == ""):
+                tk.messagebox.showwarning(title="Invalid Email or Password", message="Please input an Email or Password")
+            elif(validEmail(username)!=True):
+                tk.messagebox.showwarning(title="Invalid Email", message="Please input a valid email address")
+            elif(findUser(username)==False):
+                tk.messagebox.showwarning(title="User not Found", message="User not Found Please Create an account")
+            else:
+                ident = temp(username)
+                role = userRole(ident)
+                if(role=="Admin"):
+                    controller.show_frame(Dashboard_Admin)
+                elif(role=="Customer"):
+                    controller.show_frame(Dashboard_Customer)
+                else:
+                    controller.show_frame(Dashboard_Staff)  
+                
+class RegisterPage(tk.Frame):
+    def __init__(self, parent, controller): 
+        tk.Frame.__init__(self, parent)
+        titleLabel = ttk.Label(
+            self,
+            text="Register",
+            font=("Arial Bold",50))
+        titleLabel.place(x=1380, y=50)
+
+        userLabel = ttk.Label(
+            self,
+            text="Email:",
+            font=("Arial", 25)
+        )
+        userLabel.place(x=1300, y=200)
+
+        usernameInput = ttk.Entry(
+            self,
+            textvariable=StringVar,
+            font=("Arial", 25)
+        )
+        usernameInput.place(x=1300,y=250)
 
 
-class Dashboard(tk.Frame):
+        passwordLabel = ttk.Label(
+            self,
+            text="Password:",
+            font=("Arial", 25)
+        )
+        passwordLabel.place(x=1300, y=300)
+
+        passwordInput = ttk.Entry(
+            self,
+            textvariable=StringVar,
+            font=("Arial", 25),
+            show="•"
+        )
+        passwordInput.place(x=1300,y=350)
+
+        mIMG = tk.PhotoImage(file='./assets/branding/registerImage.png')
+        mainImg = ttk.Label(
+            self,
+            image=mIMG)
+        mainImg.image = mIMG # Prevents python clean-up destroying image
+        mainImg.pack(side="left")
+
+
+        # switch_window_button in order to call the show_frame() method as a lambda function
+        registerButton = tk.Button(
+            self,
+            text="Register",
+            command=lambda: register(self, controller, usernameInput.get(), passwordInput.get()),
+            font=("Arial", 25),
+            border=False,
+            bg="light gray",
+            relief=FLAT           
+        )
+        registerButton.place(x=1420, y=400)
+
+        loginButton = tk.Button(
+            self,
+            text="Already have an Account?",
+            command=lambda: controller.show_frame(LoginPage),
+            font=("Arial", 8, UNDERLINE),
+            fg="blue",
+            border=False,
+            relief=FLAT           
+        )
+        loginButton.place(x=1420, y=475)
+
+        def register(self, controller, username, password):
+            if(username=="" or password == ""):
+                tk.messagebox.showwarning(title="Invalid Email or Password", message="Please input an Email or Password")
+            elif(validEmail(username)!=True):
+                tk.messagebox.showwarning(title="Invalid Email", message="Please input a valid email address")
+            elif(findUser(username)==True):
+                tk.messagebox.showwarning(title="User already exists", message="User already exists")
+            else:
+                ident = temp(username)
+                role = userRole(ident)
+                if(role=="Admin"):
+                    controller.show_frame(Dashboard_Admin)
+                elif(role=="Customer"):
+                    controller.show_frame(Dashboard_Customer)
+                else:
+                    controller.show_frame(Dashboard_Staff)       
+
+class Dashboard_Admin(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Welcome to [INSERT HIFHSTREET BRAND HERE]")
+        label = tk.Label(self, text="ADMIN PAGE")
         label.pack(padx=10, pady=10)
 
         switch_window_button = tk.Button(
@@ -91,18 +228,59 @@ class Dashboard(tk.Frame):
         )
         switch_window_button.pack(side="bottom", fill=tk.X)
 
-
-class RegisterPage(tk.Frame):
+class Dashboard_Staff(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Completion Screen, we did it!")
+        label = tk.Label(self, text="STAFF PAGE")
         label.pack(padx=10, pady=10)
-        switch_window_button = ttk.Button(
-            self, text="Return to menu", command=lambda: controller.show_frame(LoginPage)
+
+        switch_window_button = tk.Button(
+            self,
+            text="Go to the Completion Screen",
+            command=lambda: controller.show_frame(LoginPage),
         )
         switch_window_button.pack(side="bottom", fill=tk.X)
 
+class Dashboard_Customer(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Customer Page")
+        label.pack(padx=10, pady=10)
 
+        switch_window_button = tk.Button(
+            self,
+            text="Go to the Completion Screen",
+            command=lambda: controller.show_frame(LoginPage),
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
+
+# Searches the db for an email address returns id
+def findUser(email):
+    pass
+
+# Takes the id and input for user and checks if the password matches input
+def passwordMatch(id,password):
+    pass
+
+def temp(username):
+    if(username=="steve@email.com"):
+        return 0
+    elif(username=="clare@email.com"):
+        return 1
+    else:
+        return 3
+# Returns users Roles e.g. Admin, Customer, Staff
+def userRole(id):
+    match id:
+        case 0:
+            return "Admin"
+        case 1:
+            return "Staff"
+        case _:
+            return "Customer"
+
+def validEmail(email):
+  return bool(re.search(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", email))
 
 if __name__ == "__main__":
     testObj = window()
